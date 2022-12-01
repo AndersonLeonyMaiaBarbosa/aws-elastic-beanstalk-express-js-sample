9dfadfad21b345e5784a8aa594a8855d25601e15
Anderson Leony <leony_maia@edu.unifor.br>
2022-11-30 21:17:32 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World (Edited)!'));
+app.get('/', (req, res) => res.send('Hello World (Step 5 - finalizar pipeline e testar)!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
